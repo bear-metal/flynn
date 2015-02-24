108cf29315b9e7de58376b71e86106b3865c5588
Ben Burkert <ben@benburkert.com>
2015-02-23 17:04:51 -0800

    host: send logs to logaggregator
    Send container logs logaggregator as RFC5424 formatted syslog messages
    over TCP. Individual lines are read from the container's STDOUT & STDERR
    FD, converted to a syslog message, and sent over a single TCP connection
    to the logaggregator leader. Log messages are buffered before being sent
    over the TCP connection so that congestion does not cause the follow
    method to block. If the buffer is full, the follower drops new messages.
    
    Signed-off-by: Ben Burkert <ben@benburkert.com>

--- host/host.go
+++ host/host.go
@@ -14,6 +14,7 @@ import (
 	"github.com/flynn/flynn/discoverd/client"
 	"github.com/flynn/flynn/host/cli"
 	"github.com/flynn/flynn/host/config"
+	"github.com/flynn/flynn/host/logmux"
 	"github.com/flynn/flynn/host/sampi"
 	"github.com/flynn/flynn/host/types"
 	"github.com/flynn/flynn/host/volume"
@@ -176,9 +177,11 @@ func runDaemon(args *docopt.Args) {
 		shutdown.Fatal(err)
 	}
 
+	mux := logmux.New(1000)
+
 	switch backendName {
 	case "libvirt-lxc":
-		backend, err = NewLibvirtLXCBackend(state, vman, legacyVolPath, "/tmp/flynn-host-logs", flynnInit)
+		backend, err = NewLibvirtLXCBackend(state, vman, legacyVolPath, "/tmp/flynn-host-logs", flynnInit, mux)
 	default:
 		log.Fatalf("unknown backend %q", backendName)
 	}
@@ -213,6 +216,7 @@ func runDaemon(args *docopt.Args) {
 		vman:         vman,
 	}
 
+	// connect to discoverd
 	discURL := os.Getenv("DISCOVERD")
 	var disc *discoverd.Client
 	if manifestFile != "" {
@@ -263,6 +267,11 @@ func runDaemon(args *docopt.Args) {
 		shutdown.Fatal(err)
 	}
 	shutdown.BeforeExit(func() { hb.Close() })
+
+	if err := mux.Connect(disc, "flynn-logaggregator-syslog"); err != nil {
+		shutdown.Fatal(err)
+	}
+	shutdown.BeforeExit(func() { mux.Close() })
 
 	cluster, err := cluster.NewClient()
 	if err != nil {
